84a40ee7d9a23c7ebdc04783b9a212d5f942d7e6
PZajac9711 <patzaj9711@gmail.com>
2020-05-10 21:00:08 +0200

    rework regular expresions test

--- src/main/java/org/zdjecia/model/expresions/RegularExpresions.java
+++ src/main/java/org/zdjecia/model/expresions/RegularExpresions.java
@@ -13,7 +13,7 @@ public interface RegularExpresions {
     }
 
     static boolean validUserEmail(String email){
-        Pattern pattern = Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
+        Pattern pattern = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
         return pattern.matcher(email).matches();
     }
 }
--- src/test/java/tests/RegularExpresionsTest.java
+++ src/test/java/tests/RegularExpresionsTest.java
@@ -1,29 +1,81 @@
 package tests;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.zdjecia.model.expresions.RegularExpresions;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-@SpringBootTest(classes = RegularExpresions.class)
-@RunWith(SpringRunner.class)
 public class RegularExpresionsTest {
 
 
-    @Test
-    public void validUserNameTest(){
-        //True
-        assertTrue(RegularExpresions.validUserName("ala"));
-        assertTrue(RegularExpresions.validUserName("kicaj123"));
-        assertTrue(RegularExpresions.validUserName("kamil999"));
-        //False
-        assertFalse(RegularExpresions.validUserName("@ala"));
-        assertFalse(RegularExpresions.validUserName("al"));
-        assertFalse(RegularExpresions.validUserName("bon!ob"));
-        assertFalse(RegularExpresions.validUserName("QW_we_q"));
+    @ParameterizedTest
+    @CsvSource({
+            "ala",
+            "kicaj123",
+            "kamil999",
+            "kAsKa9999",
+            "wojtek321",
+            "kamilPL1997"
+    })
+    public void validUserNameShouldReturnTrue(String login) {
+        try {
+            assertTrue(RegularExpresions.validUserName(login));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "al",
+            "b!un",
+            "kamCio@",
+            "a",
+            "a@#",
+            "!patryk",
+            "?????",
+    })
+    public void validUserNameShouldReturnFalse(String login) {
+        try {
+            assertFalse(RegularExpresions.validUserName(login));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "simple@example.com",
+            "very.common@example.com",
+            "disposable.style.email.with+symbol@example.com",
+            "other.email-with-hyphen@example.com",
+            "fully-qualified-domain@example.com",
+            "admin@mailserver1",
+            "example@s.example",
+    })
+    public void validEmailShouldReturnTrue(String email) {
+        try {
+            assertTrue(RegularExpresions.validUserEmail(email));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Abc.example.com",
+            "A@b@c@example.com",
+            "a\"b(c)d,e:f;g<h>i[j\\k]l@example.com",
+            "just\"not\"right@example.com",
+            "this is\"not\\allowed@example.com",
+    })
+    public void validEmailShouldReturnFalse(String email) {
+        try {
+            assertFalse(RegularExpresions.validUserEmail(email));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
